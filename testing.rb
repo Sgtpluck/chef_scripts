97b663866c3cdf2abbe24ac1ef46838f26483aec
Davida <davidamarion@gmail.com>
2014-05-13 12:49:06 -0700

    adds max/min times as well as a failsafe for only the command

--- testing.rb
+++ testing.rb
@@ -2,6 +2,7 @@ require 'mixlib/shellout'
 
 def test_command(args)
 	times = []
+	args[1] = 1 if args.count == 1
 	args[1].to_i.times do
 		start = Time.now 
 		command = Mixlib::ShellOut.new(args[0])
@@ -9,13 +10,15 @@ def test_command(args)
 		finish = Time.now
 		times << (finish - start)
 	end
-	puts times
-	puts "Average time is #{average(times)} seconds"
+	puts "Average time is #{average(times).round(4)} seconds"
+	puts "Peak time is #{times.max} seconds"
+	puts "Fastest time is #{times.min} seconds"
 end
 
 def average(times)
 	times.inject(:+) / times.length
 end
+
 
 if __FILE__ == $0
   test_command(ARGV)
