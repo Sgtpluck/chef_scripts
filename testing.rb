4c014a684e468d5beb70ed4a167b9d1048adc5c9
Davida <davidamarion@gmail.com>
2014-05-13 15:06:41 -0700

    adds remove intervals arg

--- testing.rb
+++ testing.rb
@@ -1,18 +1,42 @@
 require 'mixlib/shellout'
 
+def print_usage(args)
+	puts "Usage:\n\t#{$0} query :: <command> | <run_iterations> [remove_iterations]\n"
+end
+
 def test_command(args)
-	times = []
-	args[1] = 1 if args.count == 1
-	args[1].to_i.times do
+	if args.length < 1 
+    	print_usage(args)
+    	exit 1
+  	end
+  	
+  	run_iterations = 1
+	@times = []
+	run_iterations = args[1].to_i if args[1]
+
+	print_results(timing_test(args[0],run_iterations,args[2]))
+end
+
+def timing_test(args,iterations,remove_iterations)
+	iterations.times do
 		start = Time.now 
 		command = Mixlib::ShellOut.new(args[0])
 		command.run_command
 		finish = Time.now
-		times << (finish - start)
+		@times << (finish - start)
 	end
-	puts "Average time is #{average(times).round(4)} seconds"
-	puts "Peak time is #{times.max} seconds"
-	puts "Fastest time is #{times.min} seconds"
+	@times.slice!(remove_iterations.to_i..-1)
+end
+
+def print_results(times)
+	if times.count > 1
+		times.each {|time| puts "#{time} seconds" }
+		puts "Average runtime is #{average(times).round(4)} seconds"
+		puts "Slowest runtime is #{times.max} seconds"
+		puts "Fastest runtime is #{times.min} seconds"
+	else
+		puts "#{times.join} seconds"
+	end
 end
 
 def average(times)
